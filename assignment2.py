fe0ba1ab360d1d1a10e3e2d1a39d94c2d0a426fa
balwantrawat777 <balwantrawat333@gmail.com>
2018-10-01 00:30:17 +0530

    second assignment completed

---
+++ assignment2.py
@@ -0,0 +1,28 @@
+#Q.no.1
+print("This world is beautiful")
+
+#Q.no.2
+print("Acad"+"View")
+
+#Q.no.3
+x=int(input("enter the value of x:"))
+y=int(input("enter the value of y:"))
+z=int(input("enter the value of z:"))
+print("x","=",x,"y","=",y,"z","=",z)
+
+#Q.no.4
+print("let's get started")
+
+#Q.no.5
+s= "Acadview"
+course="Python"
+fees=5000
+print("s","=",s)
+print("course","=",course)
+print("fees","=",fees)
+
+#Q.no.6
+r=int(input("enter the radius of the circle:"))
+pi= 3.14
+area=(pi*r*r)
+print("area=",area)
